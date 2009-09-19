d771801da9e33d07cfd2feea140c7227c0beb22c
samikc <samikc@92ce1c12-94e2-11de-97c9-d5744c8abab9>
2009-09-19 16:48:44 +0000

    Adding context implementation and test cases

--- trunk/src/org/jrestful/core/context/RestServiceContext.java
+++ trunk/src/org/jrestful/core/context/RestServiceContext.java
@@ -18,6 +18,7 @@
 package org.jrestful.core.context;
 
 import java.net.URL;
+import java.util.Map;
 
 import org.jrestful.core.Constants;
 
@@ -30,28 +31,32 @@ public class RestServiceContext {
 	private String serviceName;
 	private String serviceMethod;
 	private String serviceQueryString;
-	private String path;
+	private String jrestfulWebContextPath;
+	private Map<String, String> paramsMap;
 	
 	public RestServiceContext(URL url) {
-		this.path = url.getPath();
+		this.jrestfulWebContextPath = this.setJrestFulPath(url.getPath());
 		this.serviceQueryString = url.getQuery();
 		this.setServiceName();
+		this.setServiceMethod();
 	}
 	
-	private void setServiceName() {
-		
-	}
-	
-	private void setServiceMethod() {
-		
-	}
-	/***
-	 * This method returns the service name as represented in the URL.
-	 * If no service name is found it returns null
-	 * @return The service name (if found else returns null) 
+	/**
+	 * @param path
+	 * @return
 	 */
-	public String getServiceName() {
-		String[] splits = this.path.split("/");
+	private String setJrestFulPath(String path) {
+		// TODO Auto-generated method stub
+		int i = path.indexOf(Constants.JRESTFUL_URL_CONTEXT);
+		String ret = null;
+		if ( i > -1 ) {
+			ret = path.substring(i);
+		}
+		return ret;
+	}
+
+	private void setServiceName() {
+		String[] splits = this.jrestfulWebContextPath.split("/");
 		// We would like to find the context for JRESTFUL and 
 		// the next string should be the string for service name
 		int i = 0;
@@ -68,11 +73,59 @@ public class RestServiceContext {
 		if (splits.length > i) {
 			ret = splits[i];
 		}
-		return ret;
+		this.serviceName = ret;
+		
 	}
 	
-/*	public String getPath() {
-		return this.path;
+	private void setServiceMethod() {
+		String ret = null;
+		if (this.serviceName != null) {
+			String[] splits = this.jrestfulWebContextPath.split("/");
+			// We would like to find the service name for JRESTFUL and 
+			// the next string should be the string for service method
+			int i = 0;
+			for (String s : splits) {
+				if (s.equals(this.serviceName)) {
+					i++;
+					break;
+				} else {
+					i++;
+				}
+			}
+			
+			if (splits.length > i) {
+				ret = splits[i];
+			}
+		}
+		this.serviceMethod = ret;
 	}
-*/
+	/***
+	 * This method returns the service name as represented in the URL.
+	 * If no service name is found it returns null
+	 * @return The service name (if found else returns null) 
+	 */
+	public String getServiceName() {
+		return this.serviceName;
+	}
+	/***
+	 * This method returns the service method name as represented in the URL.
+	 * If no service name is found it returns null
+	 * @return The service method name (if found else returns null) 
+	 */
+	
+	public String getServiceMethod() {
+		return this.serviceMethod;
+	}
+	
+	/**
+	 * This method returns the query string that is passed in the URL
+	 * @return
+	 */
+	public String getServiceQueryString() {
+		return this.serviceQueryString;
+	}
+	
+	public String getJrestfulWebContextPath() {
+		return this.jrestfulWebContextPath;
+	}
 }
